0f348be217d416faf3b50ca5d3d14d6a3f8eec3d
Ajay kumar <123147364+gray9-6@users.noreply.github.com>
2025-02-03 19:30:27 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,19 +3,17 @@ import React from "react";
 import './../styles/App.css';
 
 const App = () => {
-   const[value,setValue] = useState(0);
+   const [count,setCount] = useState(0)
 
-    const increaseValue = () =>{
-        setValue(value + 1);
-    }
-
-
-    return(
-        <div>
-            <p>Button clicked {value} times</p>
-            <button onClick={increaseValue}>Click me</button>
-        </div>
-    )
+  const onAdd = ()=>{
+    setCount(count+1)
+  }
+  return (
+    <div >
+    <p>Button clicked {count} times</p>
+    <button onClick={onAdd}>Click me</button>
+    </div>
+  )
 }
 
 export default App
