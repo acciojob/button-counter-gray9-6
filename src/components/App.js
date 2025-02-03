7728428c969d0b2f37ddb811a49fa84d3baaa499
Ajay kumar <123147364+gray9-6@users.noreply.github.com>
2025-02-03 19:24:33 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,11 +3,19 @@ import React from "react";
 import './../styles/App.css';
 
 const App = () => {
-  return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
+   const[value,setValue] = useState(0);
+
+    const increaseValue = () =>{
+        setValue(value + 1);
+    }
+
+
+    return(
+        <div>
+            <p>Button clicked {value} times</p>
+            <button onClick={increaseValue}>Click me</button>
+        </div>
+    )
 }
 
 export default App
